b05fc95b4c0c6963b7cf4f67a5630f2061dd5af5
Drew Taylor <drewtaylorm@gmail.com>
2019-10-26 05:55:18 -0400

    graph works

--- interpretData.py
+++ interpretData.py
@@ -1,29 +1,29 @@
 import serial
 import sys
 from time import sleep
+import datetime as dt
 import os
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 alt=0
-barom=1
 #GPS
-GPS_LA=2
-GPS_LO=3
+GPS_LA=1
+GPS_LO=2
 #gyroscope
-Gx=4
-Gy=5
-Gz=6
+Gx=3
+Gy=4
+Gz=5
 #accelerometer
-Ax=7
-Ay=8
-Az=9
+Ax=6
+Ay=7
+Az=8
 #magnemometer
-Mx=10
-My=11
-Mz=12
+Mx=9
+My=10
+Mz=11
 #time
-t=13
+t=12
 
 #launch vars
 TOL_Launch=10
@@ -51,20 +51,26 @@ TOL_Landed=1
 #list of last 10 data readings
 recentData=[]
 
-xs=[]
-ys=[]
 
 currAlt=0
+currAy=0
 
 #plt.style.use('fivethirtyeight')
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
+ax2 = ax1.twinx()
 x_vals = []
 y_vals = []
+y2_vals = []
 
-ser=serial.Serial('/dev/tty.usbserial-1410',9600)
-
-
+ser=serial.Serial('/dev/cu.usbserial-1420',9600)
+#ser=serial.Serial('/dev/tty.usbserial-1410',9600)
+'''
+while True:
+    if (ser.in_waiting>0):
+        line = ser.readline().decode('utf-8')[:-1]
+        print(line.split())
+'''
 
 def AddDataLine(dataLine):
     if len(recentData)>=10:
@@ -102,6 +108,8 @@ def CheckApogee():
 def CheckMainDeployed():
     global apogeeReached
     global mainDeployed
+    global mainConfidence
+    global minCertaintyMain
     if not apogeeReached:
         return
     currVelocity=(recentData[-1][alt]-recentData[-2][alt])/(recentData[-1][t]-recentData[-2][t])
@@ -123,11 +131,12 @@ def CheckLanded():
 def animate(i):
     global ser
     global currAlt
+    global currAy
     while (ser.in_waiting < 1):
         pass
     line = ser.readline().decode('utf-8')[:-1]
     strData = line.split()
-    if (len(strData) < 2):
+    if (len(strData) < 10):
         return
     print(line)
     data=[float(i) for i in strData]
@@ -144,10 +153,20 @@ def animate(i):
         CheckLanded()
     print 'has launched: %s\nmbo: %s\napogee reached: %s\nmain deployed: %s\nhas landed: %s\n' % (hasLaunched, mbo, apogeeReached, mainDeployed, hasLanded)
     currAlt= 0.1 * data[alt] + 0.9 * currAlt
+    currAy=0.1 + data[Ay] + 0.9 * currAy
     x_vals.append(data[t])
     y_vals.append(currAlt)
+    y2_vals.append(currAy)
     ax1.cla()
-    ax1.plot(x_vals, y_vals)
+    ax2.cla()
+    ax1.set_title(dt.datetime.now().strftime("%Y-%m-%d_%H.%M.%S"))
+    color = 'r'
+    ax1.set_xlabel('time (s)')
+    ax1.set_ylabel('altitude (m)',color=color)
+    ax1.plot(x_vals, y_vals,color=color)
+    color = 'g'
+    ax2.set_ylabel('vertical acceleration (m/s^2)',color=color)
+    ax2.plot(x_vals,y2_vals,color=color)
    
 ani = FuncAnimation(plt.gcf(), animate, interval = 50)
 plt.show()
